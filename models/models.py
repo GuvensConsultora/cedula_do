596a9681b7e643607527e10539aa4919f74bdbda
GuvensConsultora <119945264+GuvensConsultora@users.noreply.github.com>
2024-03-26 13:23:06 -0300

    Update models.py
    Corrigo la validación de republica dominica en la cedula

--- models/models.py
+++ models/models.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, api
 from odoo.exceptions import ValidationError
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.constrains('vat', 'country_id')
+    @api.constrains('vat')
     def check_vat_do(self):
         for record in self:
-            if record.country_id.code == 'DO' and record.vat:
-                if len(record.vat) not in [9, 11]:
-                    raise ValidationError("El número de cédula dominicano no es válido. Debe tener 9 o 11 dígitos.")
+            if record.vat and len(record.vat) not in [9, 11]:
+                raise ValidationError("El número de cédula no es válido. Debe tener 9 o 11 dígitos.")
