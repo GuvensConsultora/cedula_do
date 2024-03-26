c2bbba6db4add9c62fdb071c5fb1e22ad89be384
guvensconsultora <odoo@ampq01.odoo.com>
2024-03-26 14:26:29 +0000

    Genero cedula inicial

---
+++ controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class GitGithub.comGuvensconsultoraCedulaDo.git/(http.Controller):
+#     @http.route('/git_github.com_guvensconsultora_cedula_do.git//git_github.com_guvensconsultora_cedula_do.git/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/git_github.com_guvensconsultora_cedula_do.git//git_github.com_guvensconsultora_cedula_do.git//objects', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('git_github.com_guvensconsultora_cedula_do.git/.listing', {
+#             'root': '/git_github.com_guvensconsultora_cedula_do.git//git_github.com_guvensconsultora_cedula_do.git/',
+#             'objects': http.request.env['git_github.com_guvensconsultora_cedula_do.git/.git_github.com_guvensconsultora_cedula_do.git/'].search([]),
+#         })
+
+#     @http.route('/git_github.com_guvensconsultora_cedula_do.git//git_github.com_guvensconsultora_cedula_do.git//objects/<model("git_github.com_guvensconsultora_cedula_do.git/.git_github.com_guvensconsultora_cedula_do.git/"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('git_github.com_guvensconsultora_cedula_do.git/.object', {
+#             'object': obj
+#         })
