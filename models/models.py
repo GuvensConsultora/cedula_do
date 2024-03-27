6ca2fb9f6c62d4872bbedf61876df99e5d98fc62
GuvensConsultora <119945264+GuvensConsultora@users.noreply.github.com>
2024-03-27 17:41:19 -0300

    Update models.py
    Corregí el código que daba error en api contrains lo saque...

--- models/models.py
+++ models/models.py
@@ -4,9 +4,7 @@ from odoo.exceptions import ValidationError
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-
-    @api.constrains(self, vat)
-    def check_vat_do(self):
+    def check_vat_do(self, vat):
         for record in self:
             if record.vat and len(record.vat) not in [9, 11]:
                 raise ValidationError("El número de cédula no es válido. Debe tener 9 o 11 dígitos.")
